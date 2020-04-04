c69f5e2d0542956ded635aacea85ac75d724adb1
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-04 01:03:49 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram 
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.select do |k| 
+      k.downcase.split("").sort == word.downcase.split("").sort
+      
+    end
+  end
+  
+
+end
